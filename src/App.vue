c3fd3828d19f0d8d558f896e56c51c220bccc174
domai <1611997621@qq.com>
2023-12-05 23:25:40 +0800

    element主题色定制

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <el-button type="primary">Primary</el-button>  
     </div>
   </header>
 
--- vite.config.js
+++ vite.config.js
@@ -12,18 +12,31 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    // ...
+    //element-plus插件
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers:[
+      //1.配置elementPlus采用sass样式配色系统
+      ElementPlusResolver({ importStyle: 'sass'}),
+      ]
     }),
   ],
   resolve: {
     //实际的路径转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
