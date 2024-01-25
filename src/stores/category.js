94140e55bd8bdf75558b90860c09c7f01d118584
domai <1611997621@qq.com>
2024-01-26 00:33:03 +0800

    home-整体结构拆分以及分类的数据渲染

---
+++ src/stores/category.js
@@ -0,0 +1,18 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/layout'
+
+export const useCategoryStore = defineStore('category', () => {
+    //导航列表数据管理
+    const categoryList = ref([])
+    const getCategory = async () => {
+        const res = await getCategoryAPI()
+        console.log(res)
+        categoryList.value = res.result
+    }
+
+    return {
+        categoryList,
+        getCategory
+    }
+})
