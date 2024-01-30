e679b1265be38e3aaa5afff76e0ec85e3e0d475d
domai <1611997621@qq.com>
2024-01-31 01:28:46 +0800

    二级分类-列表无限加载实现

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -15,7 +15,7 @@ onMounted(() => {
 })
 
 const goodList = ref([])
-const repData = ref({
+const reqData = ref({
     categoryId: route.params.id,
     page: 1,
     pageSize: 20,
@@ -28,6 +28,26 @@ const getGoodList = async () => {
 onMounted(() => {
     getGoodList()
 })
+
+//tab切换回调
+const tabChange = () => {
+    reqData.value.page = 1
+    getGoodList()
+}
+
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  console.log('加载更多数据咯')
+  // 获取下一页的数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  // 加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -42,14 +62,14 @@ onMounted(() => {
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
                 <!-- 商品列表-->
-                <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"/>
+                <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
             </div>
         </div>
     </div>
