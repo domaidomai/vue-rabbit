064463a164290262daddf5a79bbe66736cbd81ea
domai <1611997621@qq.com>
2024-01-30 21:08:03 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -6,10 +6,24 @@ import request from '@/utils/http'
  * @return {*}
  */
 export const getCategoryAPI = (id) => {
-  return request({
-    url:'/category',
-    params:{
-      id
-    }
-  })
+    return request({
+        url: '/category',
+        params: {
+            id
+        }
+    })
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+    return request({
+        url: '/category/sub/filter',
+        params: {
+            id
+        }
+    })
 }
