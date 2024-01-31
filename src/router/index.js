87d95cc7d9f0015cd9e43e9d0721bab8d6ddbcfd
domai <1611997621@qq.com>
2024-01-31 17:26:41 +0800

    详情-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import layout from '@/views/layout/index.vue'
 import home from '@/views/home/index.vue'
 import category from '@/views/category/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
+import detail from '@/views/detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,10 @@ const router = createRouter({
           path: 'category/sub/:id',
           component: subCategory,
         },
+        {
+          path: 'detail/:id',
+          component: detail,
+        }
       ]
     },
     {
