8f8c204c33b98a354ad5e70f25cbd351c8b1125c
domai <1611997621@qq.com>
2024-02-03 01:15:54 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
 
@@ -23,8 +23,10 @@ getCategory().then(res=>{
 }) */
 
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 
