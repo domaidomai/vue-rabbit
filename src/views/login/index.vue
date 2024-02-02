958791ad86755657fd103aa11a44892b4912bcef
domai <1611997621@qq.com>
2024-02-03 00:40:28 +0800

    登录基础业务实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,10 @@
 <script setup>
 //表单校验（账号名+密码）
 import { ref } from 'vue'
+import { loginAPI } from '@/apis/user'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import { useRouter } from 'vue-router'
 
 //1.准备表单对象
 const form = ref({
@@ -30,6 +34,25 @@ const rules = {
         }
     ]
 }
+
+//3.获取form实例做统一校验
+const formRef = ref(null)
+const router = useRouter()
+const doLogin = () => {
+    const { account, password } = form.value
+    //调用实例方法
+    formRef.value.validate(async (valid) => {
+        //valid:所有表单都通过校验 true/false
+        if (valid) {
+            //login
+            const res = await loginAPI({ account, password })
+            // 1. 提示用户
+            ElMessage({ type: 'success', message: '登录成功' })
+            // 2. 跳转首页
+            router.replace({ path: '/' })
+        }
+    })
+}
 </script>
 
 
@@ -54,7 +77,8 @@ const rules = {
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                        <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
+                            status-icon>
                             <el-form-item prop="account" label="账户">
                                 <el-input v-model="form.account" />
                             </el-form-item>
@@ -66,7 +90,7 @@ const rules = {
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
-                            <el-button size="large" class="subBtn">点击登录</el-button>
+                            <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
                         </el-form>
                     </div>
                 </div>
